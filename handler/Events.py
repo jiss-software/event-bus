7920181757a180a155d4d5cca51194970f94f6bc
aiskov <hasado@yandex.ru>
2015-10-25 20:36:06 +0100

    Finish GET prototype

--- handler/Events.py
+++ handler/Events.py
@@ -14,14 +14,22 @@ class EventsHandler(tornado.web.RequestHandler):
     def get(self, context, channel):
         self.logger.info('Request to events list for `%s` at `%s`' % (channel, context))
 
+        # Load params and check that they are consistent
         start = self.get_query_argument('start', default=None)
+        peek = self.get_query_argument('peek', default=None)
 
+        if start and peek:
+            self.set_status(400)
+            self.write(json.dumps({'error': 'Request params are inconsistent. Please try to use them separately.'}))
+            return
+
+        # Add request conditions
         condition = {'context': context, 'channel': channel}
 
         if start:
             if not start.isdigit():
                 self.set_status(400)
-                self.write(json.dumps({'error': 'Start param should be and integer value'}))
+                self.write(json.dumps({'error': 'Start param should be and integer value.'}))
                 return
 
             self.logger.info('Start condition used: `%s`' % start)
@@ -29,13 +37,29 @@ class EventsHandler(tornado.web.RequestHandler):
 
         cursor = self.mongodb[options.db_name].find(condition)
 
-        if (yield cursor.count()) > 100:
+        # Use peek mode
+        if peek:
+            if not peek.isdigit():
+                self.set_status(400)
+                self.write(json.dumps({'error': 'Peek param should be and integer value.'}))
+                return
+
+            if (yield cursor.count()) < peek:
+                self.set_status(204)
+                return
+
+            self.logger.info('Peek index: `%s`' % peek)
+            cursor.limit(int(peek)).skip(1)
+        # Check if response not to big
+        elif (yield cursor.count()) > 100:
             self.set_status(400)
-            self.write(json.dumps({'error': 'Requested range is to big'}))
+            self.write(json.dumps({'error': 'Requested range is to big.'}))
             return
 
+        # Execute DB request
         data = yield cursor.sort([('timestamp', ASCENDING)]).to_list(length=100)
 
+        # Format response
         result = json.dumps([(item['timestamp'], item['payload']) for item in data])
         self.logger.info('Response for events list: %s' % result)
         self.write(result)
