5a2dc3503dc8ae909deb0b44e587c46b3ee4b15b
Антон Ишков <hasado@yandex.ru>
2015-11-12 22:10:33 +0100

    Fix database name

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from handler import HealthCheckHandler, EventsHandler
 
 define("port", default=33002, help="Application port")
 define("db_address", default="mongodb://localhost:27017", help="Database address")
-define("db_name", default="Files", help="Database name")
+define("db_name", default="Events", help="Database name")
 define("max_buffer_size", default=50 * 1024**2, help="")
 define("autoreload", default=False, help="Autoreload server on change")
 
